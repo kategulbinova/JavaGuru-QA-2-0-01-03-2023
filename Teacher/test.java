7e6417f4a85678c09a08887d56002f82194c1b20
Kate <kate.gulbinova@gmail.com>
2023-03-12 18:21:39 +0200

    Updated course number

--- Teacher/test.java
+++ Teacher/test.java
@@ -2,6 +2,6 @@ package Teacher;
 class test {
     public static void main(String[] args)
     {
-        System.out.println("Hello world, JavaGuru QA 2.0 course Teacher!");
+        System.out.println("Hello world, JavaGuru QA 2.1 course Teacher!");
     }
 }
