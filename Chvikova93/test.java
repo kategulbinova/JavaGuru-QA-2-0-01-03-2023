038c0cd579d3625ee130ce78da7ff2cf6e13cf07
Kate <kate.gulbinova@gmail.com>
2023-03-12 18:10:04 +0200

    Added a Teacher, Bashar & Chvikova first test classes.

---
+++ Chvikova93/test.java
@@ -0,0 +1,8 @@
+package Chvikova93;
+
+class test {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world - Chvikova93");
+    }
+}
---
+++ Teacher/test.java
@@ -0,0 +1,7 @@
+package Teacher;
+class test {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world, Java Guru Teacher!");
+    }
+}
