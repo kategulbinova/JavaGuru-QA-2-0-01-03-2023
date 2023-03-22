7c262262403932b22d565867355fe5c1b9148e8d
Basharych <basarsalahmads@gmail.com>
2023-03-22 22:12:55 +0200

    Cat's age homework.

---
+++ src/main/java/bashar/HelloWorld.java
@@ -0,0 +1,35 @@
+package bashar;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      /*  System.out.println("Hello World!");
+        System.out.println("Hey");
+        System.out.println("Hi");*/
+       /* int x = 5;
+        int y = 10;
+        int z = 7;
+        int w = 6;
+
+        int x1 = 2;
+        int y1 = 0;
+        int z1 = 4;
+        int w1 = -2;
+
+        System.out.println((x-z)+y*y);
+        System.out.println(w/y+(x*z));
+        System.out.println((x1-z1)+y1*y1);*/
+//      System.out.println(w1/y1+(x1*z1));
+
+        /*boolean x = true;
+        boolean y = false;
+     > true*/
+
+       /* boolean x = false;
+        boolean y = true; > false*/
+        /*boolean x = true;
+        boolean y = true; > false*/
+        /*boolean x = false;
+        boolean y = false; > true*/
+//        System.out.println(!((!x||x)&&y));
+    }
+}
