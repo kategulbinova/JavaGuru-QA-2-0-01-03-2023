d10d35a3c98fb94fee08225cc08c64864444e2d1
Kate <kate.gulbinova@gmail.com>
2023-03-22 22:08:03 +0200

    Add java class that prints booolean expression value

---
+++ src/main/java/student/HelloWorld.java
@@ -0,0 +1,13 @@
+package student;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 0;
+        int z = 4;
+        int w = -2;
+
+        System.out.println( (x - z) + y * y );
+       // System.out.println( w / y + (x * z) );
+    }
+}
