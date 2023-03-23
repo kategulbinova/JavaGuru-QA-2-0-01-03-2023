2fef008b1f050b2e0d30383b9fadd0afbb4334be
SvetlanaKussi <svetlanakussi@gmail.com>
2023-03-23 10:20:43 +0200

    Homework4 update

--- src/main/java/svetlanakussi/Homework4.java
+++ src/main/java/svetlanakussi/Homework4.java
@@ -3,7 +3,7 @@ package svetlanakussi;
 public class Homework4 {
     public static void main(String[] args)
     {
-        /* Home work steps before code writing. 1. Create new Java class. 2. Copy main method from teacher map. */
+        /* Homework steps before code writing. 1. Create new Java class. 2. Copy main method from teacher map. */
         System.out.println("Homework 4");
         // our homework variables
         int myAge;
@@ -16,7 +16,7 @@ public class Homework4 {
         System.out.println("My cats Simon age - " + catAge);
         System.out.println("Lets calculate my cat’s age in «human» years.");
         System.out.println("«Human» years based on assumption that one cat’s year equals to 7 years of a human.");
-        // add new varible
+        // add new variable
         int humanYears = 7;
         // finish result
         System.out.println("That mean my cat is already " + (catAge * humanYears) + " years old.");
