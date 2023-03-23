cb62938eceb0442df21152abe0312645370d9cbb
MarijaPl123 <mihalenkovamarija@gmail.com>
2023-03-23 14:55:05 +0200

    adding 4 homework

--- src/main/java/marijaplumite/Homework4.java
+++ src/main/java/marijaplumite/Homework4.java
@@ -9,8 +9,8 @@ public class Homework4 {
         int answer; // Declare variable answer. It is now Null
         System.out.println("My age: " + myAge); // Print out text «My age» + my actual age in years
         System.out.println("Cats age: " + catsAge); // Print out text «Cats age» + his actual age in years
-        answer = catsAge*7; // Initialize variable answer with value that I got from catsAge value (3) multiply 7. So answer now holds 21.
-        System.out.println("Cats age in human years = " + answer ); // Print out text «Cats age in human =» followed by what is now stored in variable answer (21)
+        answer = catsAge*7; // Initialize variable answer with value that I got from catsAge value (4) multiply 7. So answer now holds 28.
+        System.out.println("Cats age in human years = " + answer ); // Print out text «Cats age in human =» followed by what is now stored in variable answer (28)
         answer = myAge/7; // Initialize variable answer with value that I got from myAge value (26) divided 7. So answer now holds 3.
         System.out.println("My age in cats years = " + answer ); // Print out text «My age in cats years =» followed by what is now stored in variable answer (3)
     }
