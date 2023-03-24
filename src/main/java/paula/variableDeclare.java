f6a21e4be205cb92d8ac1fdb121971c01db73dc8
Paula <chvikova93@gmail.com>
2023-03-24 21:41:17 +0200

    to test commit process

---
+++ src/main/java/paula/variableDeclare.java
@@ -0,0 +1,17 @@
+package paula;
+
+public class variableDeclare {
+    public static void main(String[] args) {
+        int x = 2;
+        int y = 0;
+        int z = 4;
+        int w = -2;
+        int result1;
+        int result2;
+        result1 = (x-z)+y*y;
+        //result2 = w/y+(x*z);
+
+        System.out.println(result1);
+       // System.out.println(result2);
+    }
+}
