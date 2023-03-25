4504d24a3e724d0df275585c86b68f6dad0841af
Aleksej4ik <aleksei.krevnev@gmail.com>
2023-03-25 17:17:26 +0200

    Cat's age

---
+++ src/main/java/alekseikrevnev/CatsAge.java
@@ -0,0 +1,14 @@
+package alekseikrevnev;
+
+public class CatsAge {
+    public static void main(String[] args) {
+        //declaring variables
+        int virtualCatsAge;
+        int myAge;
+        //inputting my age and cat's age
+        virtualCatsAge=2;
+        myAge=36;
+        //calculating cat's age in cat's years assuming that one cat's year equals to 7 years of a human
+        System.out.println("Virtual cat's age is 7*2= " + 7*2);
+    }
+}
