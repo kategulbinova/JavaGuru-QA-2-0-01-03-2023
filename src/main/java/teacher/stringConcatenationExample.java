4fe399004b5fc36a63d424003173bc4298a2c547
Kate <kate.gulbinova@gmail.com>
2023-03-26 13:00:35 +0300

    Add string concatenation example

---
+++ src/main/java/teacher/stringConcatenationExample.java
@@ -0,0 +1,10 @@
+package teacher;
+
+public class stringConcatenationExample {
+    public static void main(String[] args) {
+        String name = "Jekaterina";
+        String surname = "Gulbinova";
+        System.out.println(name + " " + surname);
+        System.out.println(name.concat(" ").concat(surname));
+    }
+}
