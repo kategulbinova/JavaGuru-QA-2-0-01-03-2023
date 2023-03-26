a9bef4530b013a77f2d9f655460a9008e04f85ae
Глеб <spoke12988@gmail.com>
2023-03-26 18:17:42 +0300

    my homework

---
+++ src/main/java/gleb/HelloWorld.java
@@ -0,0 +1,14 @@
+package gleb;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myAge = 24;
+        int catAge = 5;
+
+        int catAgeInHumanYears = catAge * 7;
+
+        System.out.println("I am " + myAge + " years old.");
+        System.out.println("My cat is " + catAge + " years old, which is " + catAgeInHumanYears + " in human years.");
+    }
+}
+
