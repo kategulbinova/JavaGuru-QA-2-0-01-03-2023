807044fed4ec7c04d8432364e1ab84c7c8da85fb
Глеб <spoke12988@gmail.com>
2023-03-26 18:41:06 +0300

    my homework with comments

--- src/main/java/gleb/HelloWorld.java
+++ src/main/java/gleb/HelloWorld.java
@@ -2,12 +2,14 @@ package gleb;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //declaring variables
         int myAge = 24;
         int catAge = 5;
-
+        //my multipyer
         int catAgeInHumanYears = catAge * 7;
-
+        //printing my age
         System.out.println("I am " + myAge + " years old.");
+        //printing my cats age in humans age
         System.out.println("My cat is " + catAge + " years old, which is " + catAgeInHumanYears + " in human years.");
     }
 }
