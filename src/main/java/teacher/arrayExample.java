13060efa67da6c48e16a503679048e6d1cc380b1
Kate <kate.gulbinova@gmail.com>
2023-03-27 14:50:03 +0300

    add array example class

---
+++ src/main/java/teacher/arrayExample.java
@@ -0,0 +1,14 @@
+package teacher;
+
+public class arrayExample {
+
+    public static void main(String[] args) {
+
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for (String i : cars) {
+            System.out.println(i);
+        }
+
+    }
+}
