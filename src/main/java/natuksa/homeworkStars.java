880bd243bce617bc7a6304749ee0cfe7951a7896
Natuksa <natukssa@gmail.com>
2023-03-29 22:28:01 +0300

    Bebis age <3

---
+++ src/main/java/natuksa/homeworkStars.java
@@ -0,0 +1,12 @@
+package natuksa;
+
+public class homeworkStars {
+    public static void main(String[] args) {
+        for (int a = 0; a < 8; a++) {
+            for (int b = 0; b < 15; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
