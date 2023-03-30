f08a4451e7d49dec1bbd6b251973aaecc5099f9f
AleksejsLukins <aleksejslukins@gmail.com>
2023-03-30 17:03:27 +0300

    Commiting Homework nr 5 and some work from lesson 4

---
+++ src/main/java/aleksejs_lukins/StringSizes.java
@@ -0,0 +1,15 @@
+package aleksejs_lukins;
+
+public class StringSizes {
+    public static void main(String[] args) {
+        String hello = "Hi there";
+        String bye = "Bye bye";
+        int stringLength;
+        stringLength = hello.length();
+        System.out.println(stringLength);
+        System.out.println(hello.toLowerCase());
+        System.out.println(hello.toUpperCase());
+        System.out.println(hello.concat(bye));
+
+    }
+}
