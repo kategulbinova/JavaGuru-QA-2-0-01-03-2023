2aeba01006dea0547d5227cced28f4faf5ec38c3
MarijaPl123 <mihalenkovamarija@gmail.com>
2023-03-30 09:00:34 +0300

    Homework 5

---
+++ src/main/java/marijaplumite/classworks/StringExerciseNameSurname.java
@@ -0,0 +1,12 @@
+package marijaplumite.classworks;
+
+public class StringExerciseNameSurname {
+    public static void main(String[] args) {
+        String firstName = "Marija";
+        String lastName = "Plumite";
+System.out.println(firstName + " " + lastName);
+        System.out.println(firstName.concat ( " " + lastName));
+System.out.println(lastName + ", " + firstName);
+        System.out.println(lastName.concat ( ", " + firstName));
+    }
+}
