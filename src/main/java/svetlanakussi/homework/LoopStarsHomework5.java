1c583dee13268f247c41be9bff550fc8ce59499c
SvetlanaKussi <svetlanakussi@gmail.com>
2023-03-30 11:07:40 +0300

    Loop HW

---
+++ src/main/java/svetlanakussi/homework/LoopStarsHomework5.java
@@ -0,0 +1,15 @@
+package svetlanakussi.homework;
+
+public class LoopStarsHomework5 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        for ( a = 0; a < 8; a++) {
+            for (b = 0; b < 15; b++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
