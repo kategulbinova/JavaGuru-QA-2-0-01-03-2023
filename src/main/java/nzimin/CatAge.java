314716b69c5842dba746661d101240edc8472004
kolja <nikolayzimin95@gmail.com>
2023-03-31 13:52:03 +0300

    homework4

---
+++ src/main/java/nzimin/CatAge.java
@@ -0,0 +1,13 @@
+package nzimin;
+
+public class CatAge {
+    public static void main(String[] args) {
+        int MyAge;
+        int CatAge;
+        int CatAgeInHumanAge;
+        MyAge=27;
+        CatAge=5;
+        CatAgeInHumanAge=7;
+        System.out.println("My cat's age in human age is" +CatAge*CatAgeInHumanAge );
+    }
+}
