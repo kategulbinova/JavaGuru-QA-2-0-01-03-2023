6cf2b5d58f72003b91228d88e13f6c8b7e51c7ef
kolja <nikolayzimin95@gmail.com>
2023-03-31 14:02:20 +0300

    homework4

---
+++ src/main/java/nzimin/HelloWorld.java
@@ -0,0 +1,11 @@
+package nzimin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Java Guru");
+        System.out.println("Learning Java");
+        //all is works
+
+    }
+}
