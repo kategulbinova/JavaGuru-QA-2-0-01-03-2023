eb0bc8a925208ac013ddf27d54cf613b9eafa18b
OlgaZikova30 <olga.zikova30@gmail.com>
2023-04-02 20:50:13 +0300

    Homework5_LoopsFor

---
+++ src/main/java/olga_zikova/Homework5_LoopsFor.java
@@ -0,0 +1,13 @@
+package olga_zikova;
+
+public class Homework5_LoopsFor {
+    public static void main(String[] args) {
+
+        for (int i=0; i<8; i++){
+            for (int j=0; j<15; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
