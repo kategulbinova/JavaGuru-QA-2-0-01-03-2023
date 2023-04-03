596f56b185f8d08ab5f902f682815768cd8cb950
kristianamjodova <kristiana.mjodova@gmail.com>
2023-04-03 15:56:17 +0300

    stars new update

---
+++ src/main/java/Kristiana/Days.java
@@ -0,0 +1,30 @@
+package Kristiana;
+
+public class Days {
+    public static void main(String[] args) {
+    int weekday = 7;
+
+    if ( weekday == 1  ) {
+        System.out.println("Monday");
+    }
+    if ( weekday == 2 ) {
+        System.out.println("Tuesday");
+    }
+        if ( weekday == 3  ) {
+            System.out.println("Wednesday");
+        }
+            if ( weekday == 4  ) {
+                System.out.println("Thursday");
+            }
+                if ( weekday == 5  ) {
+                    System.out.println("Friday");
+                }
+                    if ( weekday == 6  ) {
+                        System.out.println("Saturday");
+                    }
+                        if ( weekday == 7  ) {
+                            System.out.println("Sunday");
+                        }
+    }
+    }
+
