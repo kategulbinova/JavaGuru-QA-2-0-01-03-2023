1be8f95282531e6d5aa22e712690d2198e7bc109
Ljekabsone <l.jekabsone@inbox.lv>
2023-04-03 15:20:31 +0300

    Changed HelloWorld class

--- src/main/java/lieneJekabsone/HelloWorld.java
+++ src/main/java/lieneJekabsone/HelloWorld.java
@@ -2,6 +2,6 @@ package lieneJekabsone;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome!");
+        System.out.println("Hello World!");
     }
 }
