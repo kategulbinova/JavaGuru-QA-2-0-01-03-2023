252770feb2479addd5df6cdb3581c727f2bd401a
yana <kassem.itani@gmail.com>
2023-04-04 23:23:33 +0300

    adding in-class work + homework

---
+++ src/main/java/YanaItani/Loop_concatenations_homework.java
@@ -0,0 +1,13 @@
+package YanaItani;
+
+public class Loop_concatenations_homework {
+    public static void main(String[] args) {
+        for (int a = 0; a < 8; a++) {
+            for (int b = 0; b < 15; b++) {
+
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
