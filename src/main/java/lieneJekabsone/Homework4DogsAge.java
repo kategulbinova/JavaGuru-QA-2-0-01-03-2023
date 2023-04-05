b862fdb47be14bef511b42f6f7493cd9d561dc92
Ljekabsone <l.jekabsone@inbox.lv>
2023-04-05 14:43:42 +0300

    Add some text to Homework4DogsAge

--- src/main/java/lieneJekabsone/Homework4DogsAge.java
+++ src/main/java/lieneJekabsone/Homework4DogsAge.java
@@ -2,13 +2,13 @@ package lieneJekabsone;
 
 public class Homework4DogsAge {
     public static void main(String[] args) {
-        // Declare variable myAge and input my age in years
-        int myAge = 42;
-        // Declare variable dogsAge and input my dog's age in years
+        // Declares variable myAge and inputs my age in years
+        int myAge = 41;
+        // Declares variable dogsAge and inputs my dog's age in years
         int dogsAge = 11;
-        // Calculate my dog’s age in «human» years
+        // Calculates my dog’s age in «human» years
         int dogsAgeinHuman = dogsAge * 7;
-        // Print result
+        // Prints the result
         System.out.println("My age is " + myAge);
         System.out.println("My Dog's age is " + dogsAge);
         System.out.println("My Dog's age in 'human' is " + dogsAgeinHuman);
