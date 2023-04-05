32974994e3b6cf3d16b371ceaad032c1637e29d4
Aleksej4ik <aleksei.krevnev@gmail.com>
2023-04-05 20:50:05 +0300

    Loop, Array practice

---
+++ src/main/java/AlekseiKrevnev/LoopThroughTheArrayPractice.java
@@ -0,0 +1,18 @@
+package AlekseiKrevnev;
+
+public class LoopThroughTheArrayPractice {
+    public static void main(String[] args) {
+
+        int[] numbers = {45, 87, 23, 2, -15, 67, 1005, -1235, -1076652, 0};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+        for (int i =0; i < numbers.length; i++) {
+            numbers[i]=numbers[i]+5;
+        }
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
