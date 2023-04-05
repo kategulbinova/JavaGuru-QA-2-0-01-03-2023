9d4f5a601fdebf5ccd24b06d819c94a11d42765e
Ljekabsone <l.jekabsone@inbox.lv>
2023-04-05 17:54:35 +0300

    Add some practice exercises and homeworks

---
+++ src/main/java/lieneJekabsone/Homework5Stars.java
@@ -0,0 +1,12 @@
+package lieneJekabsone;
+
+public class Homework5Stars {
+    public static void main(String[] args) {
+        for (int starString = 1; starString <= 8; starString++) {
+            for (int star = 1; star <= 15; star++) {
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
