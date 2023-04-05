cd288361e61aaa03f1c1c04d0fd8d398ceb832ba
Ljekabsone <l.jekabsone@inbox.lv>
2023-04-05 14:13:07 +0300

    Practice of Lesson 4

--- src/main/java/lieneJekabsone/HelloWorld.java
+++ src/main/java/lieneJekabsone/HelloWorld.java
@@ -2,6 +2,32 @@ package lieneJekabsone;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // print another text
+        /*System.out.println("Hello World!");
+        System.out.println("I like JavaGuru!");
+        System.out.println("It is the best teaching community in the world!");*/
+
+        float myAge;
+        myAge = 41;
+        myAge++;
+        System.out.println("Liene's age ir " + myAge + ".");
+
+        int x = 5;
+        int y = 10;
+        int z = 7;
+        int w = 6;
+        int answer;
+
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("z: " + z);
+        System.out.println("w: " + w);
+
+        System.out.println("Calculating Expressions with Multiple Operators:");
+
+        System.out.println("(x-z) + y*y = " + ((x-z) + y * y));
+        answer = (w/y+(x*z));
+
+        System.out.println("w/y + (x*z) = " + answer);
     }
 }
---
+++ src/main/java/lieneJekabsone/Homework4DogsAge.java
@@ -0,0 +1,16 @@
+package lieneJekabsone;
+
+public class Homework4DogsAge {
+    public static void main(String[] args) {
+        // Declare variable myAge and input my age in years
+        int myAge = 42;
+        // Declare variable dogsAge and input my dog's age in years
+        int dogsAge = 11;
+        // Calculate my dog’s age in «human» years
+        int dogsAgeinHuman = dogsAge * 7;
+        // Print result
+        System.out.println("My age is " + myAge);
+        System.out.println("My Dog's age is " + dogsAge);
+        System.out.println("My Dog's age in 'human' is " + dogsAgeinHuman);
+    }
+}
