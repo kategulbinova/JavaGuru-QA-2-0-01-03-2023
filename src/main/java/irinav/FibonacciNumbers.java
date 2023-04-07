acc36f2c311024e9744d1a181313ca7ae64dcc36
avkis <irina.verbicka@inbox.lv>
2023-04-07 21:43:33 +0300

    Adding my homework Fibonacci Numbers to the repository.

---
+++ src/main/java/irinav/FibonacciNumbers.java
@@ -0,0 +1,22 @@
+package irinav;
+
+public class FibonacciNumbers {
+    public static void main(String[] args) {
+
+        int[] arr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};  //array with 11 zeros;
+        arr[0] = 1;                                     //the first number = 1;
+        arr[1] = 1;                                     // the second number also = 1;
+
+        for (int i = 2; i < arr.length; i++) {          // loop
+            arr[i] = arr[i - 1] + arr[i - 2];
+                                                          // finding the previous element as teacher suggested;
+        }
+        printArray(arr);
+    }
+        public static void printArray(int[]arr){
+        for (int i = 0; i < arr.length; i++){           // the second loop
+
+            System.out.print(arr[i] + " ");              // to print out numbers with a space between them;
+    }
+       }
+    }
