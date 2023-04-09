2dd21ba95ae0bb6c6b08fb40f282244fdf19d77a
MarijaPl123 <mihalenkovamarija@gmail.com>
2023-04-09 19:41:06 +0300

    Homework 6

---
+++ src/main/java/marijaplumite/HomeworkFibonacci.java
@@ -0,0 +1,22 @@
+package marijaplumite;
+
+public class HomeworkFibonacci {
+    static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {0,0,0,0,0,0,0,0,0,0,0};
+        array[0] = 1;
+        array[1] = 1;
+        for (int i = 2; i < array.length; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+
+        printArray(array);
+
+    }
+}
