c79aa50789eef2912cac41eee8e85e2a5da54b18
ValerijaDavidova <valerijaxxdavidova@gmail.com>
2023-04-10 17:09:06 +0300

    Practice Day of the Week

---
+++ src/main/java/valerija/WeekDays.java
@@ -0,0 +1,29 @@
+package valerija;
+
+public class WeekDays {
+        public static void main(String[] args) {
+            int weekDay = 4;
+
+            if (weekDay== 1) {
+                System.out.println("Monday");
+            }
+            if (weekDay== 2) {
+                System.out.println("Tuesday");
+            }
+            if (weekDay== 3) {
+                System.out.println("Wednesday");
+            }
+            if (weekDay== 4) {
+                System.out.println("Thursday");
+            }
+            if (weekDay== 5) {
+                System.out.println("Friday");
+            }
+            if (weekDay== 6) {
+                System.out.println("Saturday");
+            }
+            if (weekDay== 7) {
+                System.out.println("Sunday");
+            }
+        }
+}
