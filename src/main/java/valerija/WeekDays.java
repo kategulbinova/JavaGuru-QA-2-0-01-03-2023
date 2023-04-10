6b100635f252206a43cb57ba60d634efd680d36e
ValerijaDavidova <valerijaxxdavidova@gmail.com>
2023-04-10 18:39:33 +0300

    Practice Day of the Week

--- src/main/java/valerija/WeekDays.java
+++ src/main/java/valerija/WeekDays.java
@@ -2,28 +2,26 @@ package valerija;
 
 public class WeekDays {
         public static void main(String[] args) {
-            int weekDay = 4;
+            int weekDay = 11;
+            String dayName;
 
-            if (weekDay== 1) {
-                System.out.println("Monday");
+            switch (weekDay) {
+                case 1:
+                    dayName = "Monday"; break;
+                case 2:
+                   dayName = "Tuesday"; break;
+                case 3:
+                    dayName = "Wednesday"; break;
+                case 4:
+                    dayName = "Thursday"; break;
+                case 5:
+                    dayName = "Friday"; break;
+                case 6:
+                    dayName = "Saturday"; break;
+                case 7:
+                    dayName = "Sunday"; break;
+                default: dayName = "unknown day"; break;
             }
-            if (weekDay== 2) {
-                System.out.println("Tuesday");
-            }
-            if (weekDay== 3) {
-                System.out.println("Wednesday");
-            }
-            if (weekDay== 4) {
-                System.out.println("Thursday");
-            }
-            if (weekDay== 5) {
-                System.out.println("Friday");
-            }
-            if (weekDay== 6) {
-                System.out.println("Saturday");
-            }
-            if (weekDay== 7) {
-                System.out.println("Sunday");
-            }
+            System.out.println("Today is " + dayName);
         }
 }
