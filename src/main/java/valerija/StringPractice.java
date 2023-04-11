ba5c0d51d28781ffbee4441000e36643542268ac
ValerijaDavidova <valerijaxxdavidova@gmail.com>
2023-04-11 19:54:36 +0300

    Practice String

---
+++ src/main/java/valerija/StringPractice.java
@@ -0,0 +1,9 @@
+package valerija;
+
+public class StringPractice {
+        public static void main(String[] args) {
+            String name = "Valerija";
+            String surname = "Davidova";
+            System.out.println( name.concat( " ").concat(surname));
+        }
+}
