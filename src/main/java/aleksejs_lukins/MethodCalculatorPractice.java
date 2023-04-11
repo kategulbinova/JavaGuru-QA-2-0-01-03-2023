f64a8f3c8e69a93dd9cde63279124da8cfd7997b
AleksejsLukins <aleksejslukins@gmail.com>
2023-04-11 21:16:54 +0300

    Commiting Homework nr 6 with arrays and methods

---
+++ src/main/java/aleksejs_lukins/MethodCalculatorPractice.java
@@ -0,0 +1,31 @@
+package aleksejs_lukins;
+
+public class MethodCalculatorPractice {
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+    public static int substraction(int a, int b) {
+        return a - b;
+    }
+    public static int division(int a, int b) {
+        return a / b;
+    }
+    public static void main(String[] args) {
+        int x = 125;
+        int y = 13;
+
+        System.out.println(x + " + " + y + " = " + addition(x, y));
+        System.out.println(x + " * " + y + " = " + multiplication(x, y));
+        System.out.println(x + " - " + y + " = " + substraction(x, y));
+        System.out.println(x + " / " + y + " = " + division(x, y));
+
+        int z;
+
+        z = multiplication( multiplication(x, y), addition(x, y) );
+
+        System.out.println("What is this number? " + z);
+    }
+}
