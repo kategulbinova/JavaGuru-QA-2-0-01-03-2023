1256c6a0766c22434c2d6e7280276d35356eb81e
MarijaPl123 <mihalenkovamarija@gmail.com>
2023-04-12 21:06:54 +0300

    Homework 5 rename

--- src/main/java/marijaplumite/Homework5Stars.java
+++ src/main/java/marijaplumite/Homework5Stars.java
@@ -1,6 +1,6 @@
 package marijaplumite;
 
-public class HomeworkStars {
+public class Homework5Stars {
     public static void main(String[] args) {
         for (int m = 0; m < 8; m++) {
             for (int p = 0; p < 15; p++) {
