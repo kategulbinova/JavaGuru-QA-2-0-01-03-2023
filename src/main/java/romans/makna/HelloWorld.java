b1cf9ac0c8ecd10705d55fe48d5e42d3fc1abe11
Romans.Makna <maknjuk@inbox.lv>
2023-04-13 13:40:43 +0300

    test

--- src/main/java/romans/makna/HelloWorld.java
+++ src/main/java/romans/makna/HelloWorld.java
@@ -2,6 +2,10 @@ package romans.makna;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        float myAge = 39f;
+        System.out.println(myAge);
+
     }
+
+
 }
