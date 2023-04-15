a0abe1aa8d8a02c7f7c16fe4156201e7cff7b89d
Germans <germans.zukovs@lspa.lv>
2023-04-16 00:24:32 +0300

    Make a new homework

---
+++ src/main/java/Germans/HomeworkCreateMethods.java
@@ -0,0 +1,25 @@
+package germans;
+
+public class HomeworkCreateMethods {
+    static void printArray(int[] arr) {
+        for (int i = 0 ; i<arr.length; i++ ){
+            System.out.println(arr[i] + "; ");
+        }
+
+    }
+    static void increaseEachElementsByFive (int[] arr){
+        for (int i = 0 ; i<arr.length; i++ ) {
+           arr[i] = arr[i] +5 ;
+        }
+    }
+    public static void main (String[] args) {
+        int[] numbers = {2, 4, 6, 7, 934, 556895, -12283, 0};
+        System.out.println("Numbers");
+        printArray(numbers);
+        System.out.println("Numbers + 5");
+        increaseEachElementsByFive(numbers);
+        printArray(numbers);
+    }
+}
+
+
