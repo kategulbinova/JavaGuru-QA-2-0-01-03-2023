fe8ea844911e7576073ee6350c771bf788d49f75
SvetlanaKussi <svetlanakussi@gmail.com>
2023-04-17 00:36:58 +0300

    Array Homework6 update

--- src/main/java/svetlanakussi/homework/FibonacciNumbersHomework6.java
+++ src/main/java/svetlanakussi/homework/FibonacciNumbersHomework6.java
@@ -1,21 +1,26 @@
 package svetlanakussi.homework;
 
 public class FibonacciNumbersHomework6 {
-    public static void main(String[] args) {
-        int[] value = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        System.out.println("First 11 Fibonacci numbers: ");
-        value[0] = 1;
-        value[1] = 1;
-        for (int i = 2; i < value.length; i++) {
-            value[i] = value[i - 1] + value[i - 2];
-        }
-        printArray (value);
-    }
-    public static void printArray(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
+   static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++){
             System.out.println(arr[i] + " ");
         }
         System.out.println();
     }
+    static void Fibonacci (int [] arr){
+        arr [0] = 1;
+        arr [1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr [i] = arr [i - 1] + arr [i - 2];
+        }
+    }
+    public static void main(String[] args) {
+        int[] value = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        System.out.println("First 11 Fibonacci numbers: ");
+        Fibonacci(value);
+        printArray (value);
+    }
+
+
 }
 
