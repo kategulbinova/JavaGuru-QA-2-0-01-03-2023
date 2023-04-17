d8e4b629b9de0f68da9aa433a0a3c33830bc2d64
OlgaZikova30 <olga.zikova30@gmail.com>
2023-04-17 20:43:45 +0300

    Homework6 corrections Homework7

---
+++ src/main/java/olga_zikova/homework7/ObjectsManagement/ProgramPerson.java
@@ -0,0 +1,30 @@
+package olga_zikova.homework7.ObjectsManagement;
+
+public class ProgramPerson {
+    public static void main(String[] args) {
+
+        Person myPerson1 = new Person("Olga", 65);
+        myPerson1.setSurname("Zikova");
+        myPerson1.getOlder();
+        myPerson1.getHired();
+        myPerson1.getFired();
+
+
+        Person myPerson2 = new Person("Elon", 51);
+        myPerson2.setSurname("Musk");
+        myPerson2.getOlder();
+        myPerson2.getHired();
+        myPerson2.getFired();
+
+        Person myPerson3 = new Person("Jhon");
+        myPerson3.setSurname("Brown");
+        myPerson3.getOlder();
+        myPerson3.getHired();
+        myPerson3.getFired();
+
+
+
+
+
+    }
+}
