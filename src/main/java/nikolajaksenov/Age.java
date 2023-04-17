5d95ed8b06ab5060ef2a767fdf94ff6800d0ba6d
aksenov-web <aksenov@inbox.lv>
2023-04-17 19:13:01 +0300

    Calculation added&

--- src/main/java/nikolajaksenov/Age.java
+++ src/main/java/nikolajaksenov/Age.java
@@ -3,13 +3,42 @@ package nikolajaksenov;
 public class Age {
 
     public static void main(String[] args) {
-        int mothersAge;
-        mothersAge = 60;
-        int fathersAge;
-        fathersAge = 90;
+        int wifeAge;
+        wifeAge = 35;
+        int myAge;
+        myAge = 40;
+        int diffYears;
+        diffYears = myAge - wifeAge;
 
-        System.out.print("Mother is younger for ");
-        System.out.print(fathersAge - mothersAge);
-        System.out.println(" years");
+        System.out.println("Wife is younger for " + diffYears++ + " years");
+        System.out.println("Wife is younger for " + diffYears++ + " years");
+        System.out.println("Wife is younger for " + diffYears++ + " years");
+        System.out.println();
+
+        int x = 5;
+        int y = 10;
+        int z = 7;
+        int w = 6;
+
+        int resultOne = ((x - z) + y * y);
+        int resultTwo = (w / y + (x * z));
+
+        System.out.println("Result for (x - z) + y * y) is " + resultOne );
+        if (y == 0) {
+            System.out.println("Can not devide on 0");
+        } else
+            System.out.println("Result for w / y + (x * z) is " + resultTwo );
+
+        x = 2;
+        y = 0;
+        z = 4;
+        w = -2;
+
+        System.out.println("Result for (x - z) + y * y) is " + resultOne);
+        if (y == 0) {
+            System.out.println("Can not devide on 0");
+        } else
+        System.out.println("Result for w / y + (x * z) is " + resultTwo);
+
     }
 }
