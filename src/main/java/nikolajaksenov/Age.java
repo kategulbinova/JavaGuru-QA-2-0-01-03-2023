fa6a60fd340afee1afe1944aa3e968f1a36e93b7
aksenov-web <aksenov@inbox.lv>
2023-04-17 18:33:14 +0300

    Age.java has been added.

---
+++ src/main/java/nikolajaksenov/Age.java
@@ -0,0 +1,15 @@
+package nikolajaksenov;
+
+public class Age {
+
+    public static void main(String[] args) {
+        int mothersAge;
+        mothersAge = 60;
+        int fathersAge;
+        fathersAge = 90;
+
+        System.out.print("Mother is younger for ");
+        System.out.print(fathersAge - mothersAge);
+        System.out.println(" years");
+    }
+}
