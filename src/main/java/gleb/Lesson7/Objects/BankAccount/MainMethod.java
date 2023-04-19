a1299bbb5c559ce773c9ad9e288bcae5191d22a4
Глеб <spoke12988@gmail.com>
2023-04-19 14:24:55 +0300

    my homework with comments

---
+++ src/main/java/gleb/Lesson7/Objects/BankAccount/MainMethod.java
@@ -0,0 +1,10 @@
+package gleb.Lesson7.Objects.BankAccount;
+
+public class MainMethod {
+    public static void main(String[] args) {
+       BankAccount Mine=new BankAccount();
+       Mine.Deposit();
+       Mine.CheckBalance();
+       Mine.Withdraw();
+    }
+}
