726e7687a90659cafa945648b8c0cdcf7fef15f5
kristianamjodova <kristiana.mjodova@gmail.com>
2023-04-19 18:37:13 +0300

    Person objects program homework done 1.0

---
+++ src/main/java/Kristiana/WorkWithPeople/Person.java
@@ -0,0 +1,11 @@
+package Kristiana.WorkWithPeople;
+
+public class Person {
+    String name;
+    int age;
+    boolean isMale = false;
+
+    public void getOlder() {
+        this.age++;
+    }
+}
