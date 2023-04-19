fa871e163ee9fef69dc6185bbc6236592c26c12a
kristianamjodova <kristiana.mjodova@gmail.com>
2023-04-19 14:57:50 +0300

    fibonacci homework done 2.0

---
+++ src/main/java/Kristiana/FibonacciHomework2.java
@@ -0,0 +1,25 @@
+package Kristiana;
+
+public class FibonacciHomework2 {
+    static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static void calculatingFibonacci(int[]arr){
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+    }
+    public static void main (String[]args){
+
+        int[] arr = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        arr[0] = 1;
+        arr[1] = 1;
+
+
+        calculatingFibonacci(arr);
+        printArray(arr);
+    }
+}
