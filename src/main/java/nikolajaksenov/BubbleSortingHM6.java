c7f247d5c062dc83b18a0129d063fdc4318d36ff
aksenov-web <aksenov@inbox.lv>
2023-04-20 18:54:32 +0300

    Bubble sorting reversed added.

--- src/main/java/nikolajaksenov/BubbleSortingHM6.java
+++ src/main/java/nikolajaksenov/BubbleSortingHM6.java
@@ -33,7 +33,6 @@ public class BubbleSortingHM6 {
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (array[j] < array[j + 1]) {
-                    // swap array[j] and array[j+1]
                     int temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
