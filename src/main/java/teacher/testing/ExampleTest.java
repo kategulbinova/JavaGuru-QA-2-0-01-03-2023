b43bb5bbffe895cc86c24b78bfca366cab0ad46a
Kate <kate.gulbinova@gmail.com>
2023-04-25 14:25:04 +0300

    Prepare for testing: load necessary dependencies, create a small example.

---
+++ src/main/java/teacher/testing/ExampleTest.java
@@ -0,0 +1,16 @@
+package teacher.testing;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ExampleTest {
+   // @Test
+   public static void main(String[] args) throws InterruptedException {
+
+       WebDriverManager.chromedriver().setup();
+       WebDriver driver = new ChromeDriver();
+       driver.quit();
+
+    }
+}
