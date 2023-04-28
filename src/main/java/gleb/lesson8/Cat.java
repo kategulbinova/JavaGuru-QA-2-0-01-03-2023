88f76cb998c6892d3cb8c5dad5164c3081456442
Глеб <spoke12988@gmail.com>
2023-04-28 09:56:28 +0300

    my homework with comments

---
+++ src/main/java/gleb/lesson8/Cat.java
@@ -0,0 +1,7 @@
+package gleb.lesson8;
+
+public class Cat extends AnimalDadMom {
+    String purr(){
+        return "prrr";
+    }
+}
---
+++ src/main/java/gleb/lesson8/Dog.java
@@ -0,0 +1,9 @@
+package gleb.lesson8;
+
+public class Dog extends AnimalDadMom{
+    String breed;
+    Dog(){
+        super();
+        breed="Shepard";
+    }
+}
