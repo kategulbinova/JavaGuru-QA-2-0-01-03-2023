6056d30ff51242efcf97acc87d589361b93316d8
margo <margo8ivanova@gmail.com>
2023-05-09 22:58:21 +0300

    Lecture 6 hw fibonacci upd;

--- src/main/java/margo/homeWorks/Lect6Fibonacci.java
+++ src/main/java/margo/homeWorks/Lect6Fibonacci.java
@@ -2,7 +2,8 @@ package margo.homeWorks;
 
 public class Lect6Fibonacci {
 
-    static int[] arrFib = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    /*static int[] arrFib = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
     static void printZeroFib (int[] arrFib) {
         int[] printedZeroFib = arrFib.clone();
         for (int a : printedZeroFib) {
@@ -18,11 +19,29 @@ public class Lect6Fibonacci {
         for (int i : arrFib) {
             System.out.print(i + " ");
         }
+    }*/
+    static int[] arrFib = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static void printFib(int[] arrFib) {
+        for (int i = 0; i < arrFib.length; i++) {
+            System.out.print(arrFib[i] + " ");
+        }
+        System.out.println();
+    }
+    static void calcFib (int[] arrFib) {
+        arrFib[0] = 1;
+        arrFib[1] = 1;
+        for (int i = 2; i < arrFib.length; i++) {
+            arrFib[i] = arrFib[i - 2] + arrFib[i - 1];
+        }
     }
     public static void main(String[] args) {
-        printZeroFib(arrFib);
+        /*printZeroFib(arrFib);
         System.out.println();
-        calculateAndPrintFib();
+        calculateAndPrintFib();*/
+        printFib(arrFib);
+        calcFib(arrFib);
+        printFib(arrFib);
+
     }
 }
 
