23628f81cdc7709385d88d6fe8667e853710dcd4
margo <margo8ivanova@gmail.com>
2023-05-09 23:01:16 +0300

    Lecture classwork practice

---
+++ src/main/java/margo/classWork/Lect7ObjectsPerson/MainProgram.java
@@ -0,0 +1,17 @@
+package margo.classWork.Lect7ObjectsPerson;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        Person margo = new Person();
+        margo.name = "Margarita";
+        margo.currentAge = 32;
+        margo.endSchoolAge = 19;
+        System.out.println(margo.isEmployed);
+        margo.calculateYearsAfterSchoolGraduation();
+        System.out.println(margo.name + " has finished school " + margo.yearsAfterGraduation + " years ago.");
+        margo.lastYearAge();
+        System.out.println("Last year " + margo.name + " was " + margo.currentAge);
+
+
+    }
+}
---
+++ src/main/java/margo/classWork/Lect7ObjectsPerson/Person.java
@@ -0,0 +1,18 @@
+package margo.classWork.Lect7ObjectsPerson;
+
+public class Person {
+    String name;
+    int currentAge;
+    int endSchoolAge;
+    int yearsAfterGraduation;
+    boolean isEmployed;
+    Person() {
+        this.isEmployed = true;
+    }
+    public void lastYearAge() {
+        this.currentAge--;
+    }
+    public void calculateYearsAfterSchoolGraduation() {
+        this.yearsAfterGraduation = this.currentAge - this.endSchoolAge;
+    }
+}
