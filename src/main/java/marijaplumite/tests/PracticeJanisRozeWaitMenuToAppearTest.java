bfbb9b30a6ca74ea920f5c4c8c5a5f9aa1e4b437
Kate <kate.gulbinova@gmail.com>
2023-05-14 20:27:05 +0300

    Fix deleted files

---
+++ src/main/java/marijaplumite/tests/PracticeJanisRozeWaitMenuToAppearTest.java
@@ -0,0 +1,30 @@
+package marijaplumite.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import static java.lang.Thread.sleep;
+
+public class PracticeJanisRozeWaitMenuToAppearTest {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.janisroze.lv/");
+
+        WebElement lietotajaProfilsItem = driver.findElement(By.xpath("//ul[@class='account-dropdown long']"));
+
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(lietotajaProfilsItem).build().perform();
+
+        sleep(2000);
+
+        driver.quit();
+
+    }
+}
