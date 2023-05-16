98914788af257d46c33eb0b0c7c4ef0e9793de5c
Sinekitos <n.sinickis@gmail.com>
2023-05-17 00:44:51 +0300

    Homework 11 JanisRoze

--- src/test/java/nikita/Homework11JanisRozeTestLogIn.java
+++ src/test/java/nikita/Homework11JanisRozeTestLogIn.java
@@ -1,5 +1,6 @@
 package nikita;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,7 +12,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
-import java.util.List;
 
 import static java.lang.Thread.sleep;
 public class Homework11JanisRozeTestLogIn {
@@ -22,19 +22,36 @@ public class Homework11JanisRozeTestLogIn {
 
         driver.get("https://www.janisroze.lv/");
 
+        String username = "asdx@asdx.com";
+        String password = "Java_test1%";
+
+
         Actions actions = new Actions(driver);
-        WebElement lietotajaProfilsIcon = driver.findElement(By.xpath("//a[@class='link']//span[@class='icon ic ic-user']"));
-        actions.moveToElement(lietotajaProfilsIcon).build().perform();
-
-        WebElement ielogotiesBtn = driver.findElement(By.xpath("//a[@title='Ielogoties']"));
-        WebDriverWait waitLogin = new WebDriverWait(driver, Duration.ofSeconds(2));
-        waitLogin.until(ExpectedConditions.elementToBeClickable(ielogotiesBtn));
-        ielogotiesBtn.click();
+        WebElement userProfileIcon = driver.findElement(By.xpath("//a[@class='link']//span[@class='icon ic ic-user']"));
+        actions.moveToElement(userProfileIcon).build().perform();
 
 
+        WebDriverWait waitLoginBtn = new WebDriverWait(driver, Duration.ofSeconds(2));
+        waitLoginBtn.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Ielogoties']")));
+        WebElement loginPageBtn = driver.findElement(By.xpath("//a[@title='Ielogoties']"));
+        loginPageBtn.click();
 
+        WebDriverWait waitLoginField = new WebDriverWait(driver, Duration.ofSeconds(10));
+        waitLoginField.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='login[username]']")));
+        WebElement loginField = driver.findElement(By.xpath("//input[@name='login[username]']"));
+        loginField.sendKeys(username);
 
-        sleep(5000);
+        WebElement passwordField = driver.findElement(By.xpath("//input[@name='login[password]']"));
+        passwordField.sendKeys(password);
+
+        WebElement loginBtn = driver.findElement(By.id("send2"));
+        loginBtn.click();
+
+        WebElement footerCopyrightText = driver.findElement(By.xpath("//div[@class='footer-copyright']"));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", footerCopyrightText);
+
+        //sleep(5000); //uncomment sleep to see that scrolling down to copyright text in footer works properly
 
 
         driver.quit();
