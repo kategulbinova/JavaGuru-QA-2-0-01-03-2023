dc97d3b951e28ea3eaad0c3435f56802055df545
OlgaZikova30 <olga.zikova30@gmail.com>
2023-05-24 12:35:55 +0300

    Homework12

---
+++ src/test/java/olgaZikova/homework12/sauceDemoTestHwrk/pages/InventoryPage.java
@@ -0,0 +1,26 @@
+package olgaZikova.homework12.sauceDemoTestHwrk.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class InventoryPage {
+    private WebDriver driver;
+    private String burgerMenuIconID = "react-burger-menu-btn";
+
+    public InventoryPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public boolean isOpenPage() {
+        WebElement burgerMenuIcon = driver.findElement(By.id(burgerMenuIconID));
+
+        if (burgerMenuIcon.isDisplayed()) return true;
+        else return false;
+
+
+
+    }
+}
+
+
