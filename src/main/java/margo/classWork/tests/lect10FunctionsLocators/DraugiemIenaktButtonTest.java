376affbcbe39a5c798be7eb3ea5428f8e526b99f
margo <margo8ivanova@gmail.com>
2023-05-24 23:51:44 +0300

    lecture 10 Functions and Locators classWork and homeWork

--- src/main/java/margo/classWork/tests/lect10FunctionsLocators/DraugiemIenaktButtonTest.java
+++ src/main/java/margo/classWork/tests/lect10FunctionsLocators/DraugiemIenaktButtonTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static java.lang.Thread.sleep;
-
 public class DraugiemIenaktButtonTest {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
