1353ba1b46d2e65dd8e254c7222f928370e7f4a5
kristianamjodova <kristiana.mjodova@gmail.com>
2023-05-26 20:25:32 +0300

    login homework test homework done janis roze

--- src/test/java/kristiana/JanisRozeLoginTest.java
+++ src/test/java/kristiana/JanisRozeLoginTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
 
@@ -30,7 +29,6 @@ public class JanisRozeLoginTest {
         WebDriverManager.chromedriver().setup();
         wait = new WebDriverWait(driver, Duration.ofSeconds(3));
         js = (JavascriptExecutor) driver;
-
 
         driver.get("https://www.janisroze.lv/");
 
@@ -58,7 +56,6 @@ public class JanisRozeLoginTest {
 
         WebElement Element = driver.findElement(By.xpath("//div[@class='footer-copyright']"));
         js.executeScript("arguments[0].scrollIntoView();", Element);
-
 
 
     }
