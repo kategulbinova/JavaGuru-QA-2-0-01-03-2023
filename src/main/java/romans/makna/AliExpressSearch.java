de39174cccf395dc63ae28bb1b3442b84ecf7971
maknjuk <maknjuk@inbox.lv>
2023-06-09 23:14:39 +0300

    111

--- src/main/java/romans/makna/AliExpressSearch.java
+++ src/main/java/romans/makna/AliExpressSearch.java
@@ -1,31 +1,47 @@
-package romans.makna;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
 public class AliExpressSearch {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
+    public static void main(String[] args) {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+
+        // Create a new instance of ChromeDriver
         WebDriver driver = new ChromeDriver();
 
+        // Navigate to AliExpress website
         driver.get("https://www.aliexpress.com/");
 
-        WebElement searchField = driver.findElement(By.xpath("//*[@id=\"search-key\"]"));
-        searchField.sendKeys("tattoo ink");
+        // Search for the keyword "tattoo"
+        String searchKeyword = "tattoo";
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='search-key']"));
+        searchInput.sendKeys(searchKeyword);
+        searchInput.submit();
 
-        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"form-searchbar\"]/div[1]/input"));
-        searchButton.click();
+        // Find all the resulting links
+        List<WebElement> linkElements = driver.findElements(By.xpath("//a[@class='item-title']"));
 
-        List<WebElement> tattooLinks = driver.findElements(By.xpath("//a[contains(@href, 'www.aliexpress.com/item')]"));
+        // Save all the links into a List
+        List<String> links = new ArrayList<>();
+        for (WebElement linkElement : linkElements) {
+            links.add(linkElement.getAttribute("href"));
+        }
 
-        System.out.println("Size of my list is " + tattooLinks.size());
+        // Print out the size of the List
+        System.out.println("Number of links: " + links.size());
 
+        // Print out all the links
+        System.out.println("Links:");
+        for (String link : links) {
+            System.out.println(link);
+        }
+
+        // Quit the driver
         driver.quit();
     }
 }
-
---
+++ src/test/java/romansmakna/MyImportantTest.java
@@ -0,0 +1,9 @@
+package romansmakna;
+import org.testng.annotations.Test;
+
+public class MyImportantTest {
+    @Test
+    public void MyTest() {
+        System.out.println("Hello test");
+    }
+}
