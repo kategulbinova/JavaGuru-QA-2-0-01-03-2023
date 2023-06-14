4ab289d1b20833d262e89f7618e6625a43e3ca55
Germans <germans.zukovs@lspa.lv>
2023-06-14 19:03:07 +0300

    Something new in homework

--- src/test/java/germans/sauceDemo/tests/LoginTest.java
+++ src/test/java/germans/sauceDemo/tests/LoginTest.java
@@ -1,6 +1,7 @@
 package germans.sauceDemo.tests;
 
 import germans.sauceDemo.pages.InventoryPage;
+import germans.sauceDemo.pages.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
@@ -8,16 +9,14 @@ import org.testng.annotations.Test;
 
 
 public class LoginTest extends BaseTest{
-    public LoginTest(WebDriver driver) {
 
-    }
     @Test
     @Parameters({"standard_user", "secret_sauce"})
-    public void successfulLoginCheck(String standard_user, String secret_sauce) {
-        LoginTest loginPage = new LoginTest(driver);
+    public void successfulLoginCheck(String standardUser, String secretSauce) {
+        LoginPage loginPage = new LoginPage(driver);
         InventoryPage inventoryPage = new InventoryPage(driver);
 
-        loginPage.successfulLoginCheck("standard_user", "secret_sauce");
+        loginPage.login(standardUser,secretSauce);
 
         Assert.assertTrue( inventoryPage.isOpen() );
     }
